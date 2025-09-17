474262777cc5ca64fe29e256e8ddfb945cdb26e8
Bhava Narayana <bhavanarayanachukka@gmail.com>
2025-09-17 20:56:11 +0530

    commited

---
+++ Internal.js
@@ -0,0 +1,5 @@
+import os from 'node:os';
+import path from 'node:path';
+console.log(os.type())
+console.log(os.version())
+console.log(path.parse("C:\Users\Bhava Narayana\Downloads\New folder\Annaya Resume.pdf"))
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-
-let { mybestfriends }=require("./First.js");
+import {i} from "./Custom.js"
+import {mybestfriends} from "./First.js"
 console.log(`my  bestfrirends name is ${mybestfriends[0]}`)
 console.log(`my  bestfrirends name is ${mybestfriends[1]}`)
+console.log(`${i}`)
